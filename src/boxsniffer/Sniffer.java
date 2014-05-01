a8cf7fdaa74b9b527c1575dc67dfc3d97f25265f
Chase Willden <chase.willden@gmail.com>
2014-05-01 10:02:16 -0600

    Sure Did

--- src/boxsniffer/Sniffer.java
+++ src/boxsniffer/Sniffer.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
